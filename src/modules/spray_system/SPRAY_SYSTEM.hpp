f81f56bff9bd3875e2a46412b889751aa3e6efa0
kishore-suyochi <kishoreacharya.suyochi@gmail.com>
2026-03-16 13:26:20 +0530

    feat(actuators): Added spray system module with pump and centrifugal nozzle support

---
+++ src/modules/spray_system/SPRAY_SYSTEM.hpp
@@ -0,0 +1,89 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2024 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+
+
+
+/******************************************************************************************************** */
+
+
+#pragma once
+
+#include <px4_platform_common/module.h>
+#include <px4_platform_common/module_params.h>
+#include <px4_platform_common/px4_work_queue/ScheduledWorkItem.hpp>
+
+#include <parameters/param.h>
+
+#include <uORB/Publication.hpp>
+#include <uORB/Subscription.hpp>
+
+#include <uORB/topics/spray_system_status.h>
+#include <uORB/topics/vehicle_status.h>
+
+#include <mathlib/mathlib.h>
+#include <uORB/topics/parameter_update.h>
+
+using namespace time_literals;
+
+class SpraySystem : public px4::ScheduledWorkItem, public ModuleBase<SpraySystem>, public ModuleParams
+{
+public:
+	SpraySystem();
+	~SpraySystem() override = default;
+
+	bool init();
+	static void stop();
+
+	static int task_spawn(int argc, char *argv[]);
+	static int custom_command(int argc, char *argv[]);
+	static int print_usage(const char *reason = nullptr);
+
+	int print_status();
+
+private:
+	uORB::Publication<spray_system_status_s> _spray_pub{ORB_ID(spray_system_status)};
+	uORB::Subscription _vehicle_status_sub{ORB_ID(vehicle_status)};
+	uORB::Subscription _parameter_update_sub{ORB_ID(parameter_update)};
+
+	bool _armed{false};
+	bool _in_auto{false};
+	int32_t _spray_mode{0};
+
+	void Run() override;
+	DEFINE_PARAMETERS(
+		(ParamFloat<px4::params::SPRAY_PUMP_LPM>) _pump_lpm,
+		(ParamFloat<px4::params::SPRAY_CENT_RPM>) _cent_rpm,
+		(ParamInt<px4::params::SPRAY_EN_MODE>) _en_mode
+	)
+};
