b053f873aef2ce5ed1bbe023ffbaba7e5ae633f0
kishore-suyochi <kishoreacharya.suyochi@gmail.com>
2026-05-12 13:19:45 +0530

    feat(ekf2): Implemented AUTO mission to HOLD mode switching on obstacle detection

--- src/modules/commander/Commander.cpp
+++ src/modules/commander/Commander.cpp
@@ -1785,7 +1785,7 @@ void Commander::handleObstacleDetection()
 			_user_mode_intention.change(
 				vehicle_status_s::NAVIGATION_STATE_AUTO_LOITER,
 				ModeChangeSource::ModeExecutor,
-				false,  // allow_fallback
+				true,  // allow_fallback
 				true    // force
 			);
 		}
